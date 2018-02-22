fa0e1dd6b254a34d6c5299f61f90ad3a4724fe21
Ravi Honnavalli <ravihr@gmail.com>
2018-02-22 12:51:33 +0800

    Update tflearn.py

--- tflearn.py
+++ tflearn.py
@@ -6,10 +6,10 @@ from tflearn.data_utils import load_csv
 
 #target_column : classification value column
 #n_classes : number of classifications
-from tflearn.datasets import titanic
-titanic.download_dataset('titanic_dataset.csv')
+from auditng.datasets import processed
+processed.download_dataset('processed_dataset.csv')
 
-data, labels = load_csv("titanic_dataset.csv", target_column=0, categorical_labels=True, n_classes=2)
+data, labels = load_csv("processed_dataset.csv", target_column=0, categorical_labels=True, n_classes=2)
 print labels
 def preprocess(data, column_to_ignore):
 	for id in sorted(column_to_ignore, reverse=True):
@@ -35,12 +35,3 @@ model = tflearn.DNN(net)
 
 model.fit(data, labels, n_epoch=10, batch_size=16, show_metric=True)
 
-
-jack = [3, 'Jack Dawson', "male", 19, 0, 0, 'N/A', 5.0000]
-kate = [1, 'Rose DeWitt Bukater', 'female', 17, 1, 2, 'N/A', 100.0000]
-
-jack, kate = preprocess([jack, kate], to_ignore)
-
-pred = model.predict([jack, kate])
-print "Dicaprio survival rate : " + str(pred[0][1])
-print "Winslet survival rate : " + str(pred[1][1])
